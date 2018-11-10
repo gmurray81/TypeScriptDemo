21647b8a4f6521606c70912bdc9f5184d145ca99
gmurray81 <gmurray@gmail.com>
2018-11-10 11:03:57 -0500

    additional stuff

--- main.js
+++ main.js
@@ -92,3 +92,90 @@ cat.think().then(function (result) {
     console.log("cat had a problem thinking");
 });
 
+
+function Shape(centerX, centerY) {
+    this.centerX = centerX;
+    this.centerY = centerX;
+}
+Shape.prototype.describe = function () {
+    console.log("This is a " + this.shapeType + ", centered at " 
+    + this.centerX + ", " + this.centerY);
+}
+Shape.prototype.area = function () {
+    throw 
+    new Error("area is not implemented");
+}
+Shape.prototype.render = function (context) {
+
+}
+
+function Circle(centerX, centerY, radius) {
+    Shape.constructor.call(this, centerX, centerY);
+    this.radius = radius;
+}
+Circle.extend(Shape);
+
+Circle.prototype.describe = function () {
+    Shape.describe() + " with radius: " + this.radius;
+}
+Circle.prototype.area = function () {
+    return
+        Math.Pi * 
+        this.radius * this.radius;
+}
+Circle.prototype.render = function (context) {
+    context.strokeStyle = "green";
+    context.fillStyle = "gray"
+    context.lineWidth = 2;
+
+    context.beginPath();
+    context.arc(this.centerX, this.centerY, this.radius, 0, Math.Pi);
+    context.endPath();
+    context.fill();
+    context.stroke();
+}
+
+function Rectangle(centerX, centerY, width, height) {
+    Shape.constructor.call(this, centerX, centerY);
+    this.width = width;
+    this.height = height;
+}
+Rectangle.extend(Shape);
+
+Rectangle.prototype.area = function () {
+    return width * height;
+}
+
+Rectangle.prototype.render = function (context) {
+    context.strokeStyle = "teal";
+    context.fillStyle = red;
+    context.fillRect(
+        this.centerX - this.width / 2.0,
+        this.centerY - this.height / 2.0,
+        this.width,
+        this.height);
+    context.strokeRect(
+        this.centerX - this.width / 2.0,
+        this.centerY - this.height / 2.0,
+        this.width);
+}
+
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2D");
+
+var rect = {
+    left: 25,
+    top: 40,
+    width: 40,
+    height: 30
+};
+
+var circle1 = new Circle(250, 250, 50);
+var circle2 = new Circle(300, 250, 30);
+var rectangle = new Reactangle(
+    rect.left + width / 2.0, 
+    rect.top + height / 2.0, 
+    rect.width, 
+    rect.height);
+
+var shapes = [circle, circle2, rect];
